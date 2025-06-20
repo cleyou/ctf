d3b8cf4710eb1be0e9fac4f3720ff36c76e6b828
cleyou <77749361+cleyou@users.noreply.github.com>
2025-06-21 01:02:22 +0200

    Create encode.py

---
+++ fonctionnel/encode.py
@@ -0,0 +1,20 @@
+from functools import reduce
+from operator import xor
+from typing import List
+
+def f(x: List[int]) -> List[int]:
+    return list(map(lambda c: c ^ 42, x))
+
+def g(x: List[int]) -> List[int]:
+    return list(reversed(x))
+
+def h(x: str) -> List[int]:
+    return list(map(lambda c: ord(c) + 1, x))
+
+def encrypt(plain: str) -> List[int]:
+    return f(g(h(plain)))
+
+# Try it
+ciphertext = encrypt(flag)
+# ciphertext = 
+# [84, 30, 28, 31, 30, 28, 74, 31, 30, 28, 27, 31, 30, 28, 31, 30, 74, 89, 122, 83, 74, 81, 72, 86, 95, 64, 4, 76, 76, 64, 94, 76]
